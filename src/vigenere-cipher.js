d3162d2e2ed594f3ec1b8e3c182263e98456e9e5
Sergey-98 <yatsenko1998@yandex.ru>
2022-05-02 00:16:46 +0400

    feat: complete all task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -26,11 +26,87 @@ class VigenereCipheringMachine {
   encrypt(message, key) {
     if (!message || !key) {
       throw new Error('Incorrect arguments!');
-    } 
+    }
+    //------------
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    //------------
+    //-------Формируем новый ключ
+    let s = 0;
+    let keyArr = [];
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        keyArr.push(key[s]);
+      s += 1;
+      if (s >= key.length) {
+        s = 0;
+      }
+      } else {
+        keyArr.push(message[i]);
+      }
+      
+    }
+    let newKey = keyArr;
+    //-------------Добавление данных в массив исходя из формулы C[i] = (P[i] + K[i]) % 26;
+    let res = [];
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let C = (this.findIndex(alphabet, message[i]) + this.findIndex(alphabet, newKey[i])) % 26;
+        res.push(alphabet[C]);
+      } else {
+        res.push(message[i]);
+      }
+    }
+    return (this.isReverse) ? res.join('') : res.reverse().join('');
   }
   decrypt(message, key) {
     if (!message || !key) {
       throw new Error('Incorrect arguments!');
+    }
+
+    //------------
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+    //------------
+
+    //-------Формируем новый ключ
+    let s = 0;
+    let keyArr = [];
+
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        keyArr.push(key[s]);
+      s += 1;
+      if (s >= key.length) {
+        s = 0;
+      }
+      } else {
+        keyArr.push(message[i]);
+      }
+      
+    }
+    let newKey = keyArr;
+    //-------------
+    //-------------Добавление данных в массив исходя из формулы P[i] = (C[i] - K[i] + 26) % 26;
+    let res = [];
+    for (let i = 0; i < message.length; i++) {
+      if (alphabet.includes(message[i])) {
+        let P = (this.findIndex(alphabet, message[i]) - this.findIndex(alphabet, newKey[i]) + 26) % 26;
+        res.push(alphabet[P]);
+      } else {
+        res.push(message[i]);
+      }
+    }
+    return (this.isReverse) ? res.join('') : res.reverse().join('');
+  }
+  //--------Метод для определения индекса элемента в алфавите
+  findIndex(alphabet, elem) {
+    for (let i = 0; i < alphabet.length; i++) {
+      if (alphabet[i] == elem) {
+        return i;
+      }
     }
   }
 }
